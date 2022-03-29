05b5efc919a4b5b0bf5bffa7665a146eb19f8736
Dede Gunawan <gunawan@dede.web.id>
2022-03-29 10:10:36 +0700

    Update

--- app/Http/Middleware/NpmMiddleware.php
+++ app/Http/Middleware/NpmMiddleware.php
@@ -18,6 +18,7 @@ class NpmMiddleware
     public function handle(Request $request, Closure $next)
     {
         $npm = '197006078';
+        dd(!$request->header('npm'), $request->header('npm') != $npm);
         if (
             !$request->header('npm') ||
             $request->header('npm') != $npm
